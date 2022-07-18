28c7d99c996f62920f4f9bb6df226886e659953b
Priyansh <priyanshkumar001@gmail.com>
2022-07-18 18:15:15 +0530

    Added Files and Folders like assets, containers, constants, components

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { About, Header, Footer, Skills, Testimonials, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
 const App = () => {
     return (
-        <h1>App</h1>
+        <div className='app'>
+            <Navbar />
+            <Header />
+            <About />
+            <Work />
+            <Skills />
+            <Testimonials />
+            <Footer />
+        </div>
     )
 }
 
-export default App;
+export default App; 
