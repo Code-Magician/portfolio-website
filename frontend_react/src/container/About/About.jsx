cc34b6fbefb7069086b8d32e6d5869d858f315c8
Priyansh <priyanshkumar001@gmail.com>
2022-07-20 01:42:43 +0530

    Completed Work and Skills section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,7 +3,7 @@ import './About.scss';
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import { client, urlFor } from '../../client';
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 
 
 
@@ -46,4 +46,8 @@ const About = () => {
     )
 }
 
-export default AppWrap(About, 'about')
+export default AppWrap(
+    MotionWrap(About, 'app__about'),
+    'about',
+    'app__whitebg',
+);
